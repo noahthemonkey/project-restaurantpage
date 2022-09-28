96d01ad8b7f9c20260be72e21a02fc92df8e456a
Noah Msekni <msekninoah@gmail.com>
2022-09-28 18:30:37 +0200

    Add font and start layout for home, add content
    Start filling out the content that will be in home and start setting
    the foundation for what the layout will be.
    
    Use grid to seperate the welcome text from the reviews

--- src/home.js
+++ src/home.js
@@ -12,24 +12,42 @@ function resetContent(){
 }
 resetContent()
 
+const homeContent = document.createElement('div')
+
+const reviewsContainer = document.createElement('div')
+
+const reviews = document.createElement('div')
+const customerReviews = document.createElement('div')
+
+homeContent.setAttribute('class', 'homeContent')
+
+reviewsContainer.setAttribute('class', 'reviewsContainer')
+
+reviews.textContent = 'BananAdvisor 9/10 Bananas!'
 
 const cafeH1 = document.createElement('h1')
-
 const cafeP = document.createElement('p')
 
 
 
 cafeH1.textContent = 'Welcome to PixCafe!'
 
-
-cafeP.textContent = 'I went to PixCafe and I had the best time of my life! I will never forget the time I spent there! 10/10 recommend it!'
-
-
+cafeP.style.fontStyle = 'italic';
+cafeP.textContent = 'I went to PixCafe and I had the best time of my life! I will never forget the time I spent there! 10/10 recommend it! - Mr Elephant'
 
 
-content.appendChild(cafeH1)
 
-content.appendChild(cafeP)
+content.appendChild(homeContent)
+homeContent.appendChild(cafeH1)
+homeContent.appendChild(reviewsContainer)
+reviewsContainer.appendChild(reviews)
+reviewsContainer.appendChild(customerReviews)
+customerReviews.appendChild(cafeP)
+
+
+
+
+
 
 
 }
